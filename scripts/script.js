c3a404b7920b2226dafe5074924787f94c856eff
Schifelbain <daniel.schifelbain@capgemini.com>
2023-12-29 10:36:08 -0300

    Created the game grid + add food

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,26 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const size = 30;
+const size = 50;
 
 const snake = [
     {x: 200, y:200},
-    {x: 230, y:200}
+    {x: 250, y:200}
 ];
+
+const food = {
+    x:90,
+    y:90,
+    color: "green"
+}
 
 let direction, loopId;
 
+const drawFood = () => {
+    ctx.fillStyle = food.color;
+    ctx.fillRect(food.x, food.y, size, size);
+
+}
 
 const drawSnake = () => {
     ctx.fillStyle = "#DDD";
@@ -45,16 +56,39 @@ const moveSnake = () => {
     snake.shift();
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#191919";
+
+    
+    for (let i = 50; i < canvas.width; i += 50){
+        ctx.beginPath();
+        ctx.lineTo(i, 0);
+        ctx.lineTo(i, 500);
+    
+        ctx.stroke();   
+        ctx.beginPath();
+        ctx.lineTo(0, i);
+        ctx.lineTo(500, i);
+    
+        ctx.stroke();    
+    }
+    
+}
+
+
 const gameLoop = () => {
     clearInterval(loopId);
 
-    ctx.clearRect(0, 0, 500, 500);
+    ctx.clearRect(0, 0, 500, 500)
+    drawGrid();
+    drawFood();
     moveSnake();
     drawSnake();
 
     loopId = setTimeout(() => {
         gameLoop();
-    }, 300)
+    }, 400)
 }
 
 gameLoop();
